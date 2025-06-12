f1e7cc37187b276bb69e0cf6a714add8868e8f31
2k4pxthanh <2k4pxthanh@gmail.com>
2025-06-12 18:46:08 +0700

    ok2

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:7979";
+export const BASE_URL = "https://beshopphone.onrender.com";
 export const API_URL = "https://beshopphone.onrender.com/api/v1";
 export const API_URL2 = "http://localhost:7979/";
